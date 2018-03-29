50d00b7bfad92acea1b5f8e25e6194c9bf0840a1
ohansemmanuel <ohansemmanuel@gmail.com>
2018-03-29 07:18:32 +0100

    use the more appropriate MovieDetails name

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,7 @@ import { Route } from "react-router-dom";
 import { Center } from "../components/Layout";
 import { StyledMovieContainer } from "../components/Styled";
 import Body from "./Body";
-import Movie from "./Movie";
+import MovieDetails from "./MovieDetails";
 import Header from "../components/Header";
 import theme from "../constants/theme";
 
@@ -13,7 +13,7 @@ const App = props => {
       <StyledMovieContainer>
         <Header />
         <Route exact path="/" component={Body} />
-        <Route path="/movies/:id" component={Movie} />
+        <Route path="/movies/:id" component={MovieDetails} />
       </StyledMovieContainer>
     </Center>
   );
