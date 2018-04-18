86376782f9515738ff8986e29791eeb7b0e6bf19
ohansemmanuel <ohansemmanuel@gmail.com>
2018-04-18 13:21:36 +0100

    fix bug with attempting to load redux devtools in prod

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
-import Root from "./containers/Root.dev";
+import Root from "./containers/Root";
 import configureStore from "./store/configureStore";
 import registerServiceWorker from "./registerServiceWorker";
 
